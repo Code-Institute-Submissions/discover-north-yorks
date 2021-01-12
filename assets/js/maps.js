f257101a6a4c3405df53e16f9dd97571377b705a
PaulFrankling <franky.p@hotmail.co.uk>
2021-01-12 16:13:32 +0000

    set up google maps and applied api key, as well as js file and applying the longitude, latitude and zoom onto north yorkshire

---
+++ assets/js/maps.js
@@ -0,0 +1,8 @@
+function initMap() {
+    let map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 54.250359,
+            lng: -1.470855},
+            zoom: 8
+    });
+}   
