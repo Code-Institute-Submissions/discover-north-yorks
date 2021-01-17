18c97037af494c4f28fc42b68dd8eef8b7ec1e69
PaulFrankling <franky.p@hotmail.co.uk>
2021-01-17 19:31:02 +0000

    added first marker to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -8,105 +8,57 @@ function initMap() {
     {
         "featureType": "landscape",
         "stylers": [
-            {
-                "hue": "#FFA800"
-            },
-            {
-                "saturation": 0
-            },
-            {
-                "lightness": 0
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
+            {"hue": "#FFA800"},
+            {"saturation": 0},
+            {"lightness": 0},
+            {"gamma": 1}
+        ]},
     {
         "featureType": "road.highway",
         "stylers": [
-            {
-                "hue": "#53FF00"
-            },
-            {
-                "saturation": -73
-            },
-            {
-                "lightness": 40
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
+            {"hue": "#53FF00"},
+            {"saturation": -73},
+            {"lightness": 40},
+            {"gamma": 1}
+        ]},
     {
         "featureType": "road.arterial",
         "stylers": [
-            {
-                "hue": "#FBFF00"
-            },
-            {
-                "saturation": 0
-            },
-            {
-                "lightness": 0
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
+            {"hue": "#FBFF00"},
+            {"saturation": 0},
+            {"lightness": 0},
+            {"gamma": 1}
+        ]},
     {
         "featureType": "road.local",
         "stylers": [
-            {
-                "hue": "#00FFFD"
-            },
-            {
-                "saturation": 0
-            },
-            {
-                "lightness": 30
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
+            {"hue": "#00FFFD"},
+            {"saturation": 0},
+            {"lightness": 30},
+            {"gamma": 1}
+        ]},
     {
         "featureType": "water",
         "stylers": [
-            {
-                "hue": "#00BFFF"
-            },
-            {
-                "saturation": 6
-            },
-            {
-                "lightness": 8
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
+            {"hue": "#00BFFF"},
+            {"saturation": 6},
+            {"lightness": 8},
+            {"gamma": 1}
+        ]},
     {
         "featureType": "poi",
         "stylers": [
-            {
-                "hue": "#679714"
-            },
-            {
-                "saturation": 33.4
-            },
-            {
-                "lightness": -25.4
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    }
-]
+            {"hue": "#679714"},
+            {"saturation": 33.4},
+            {"lightness": -25.4},
+            {"gamma": 1}
+        ]}
+       ]
     });
-}   
+        
+    const myMark = {"lat": 54.1218, "lng": -0.9061};
+
+    const placeMarker = new google.maps.Marker({position: myMark});
+
+    placeMarker.setMap(map);
+}
