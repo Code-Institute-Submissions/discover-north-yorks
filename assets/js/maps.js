7658694065ad3281854adf23a5846175a2a8063a
PaulFrankling <franky.p@hotmail.co.uk>
2021-01-23 15:49:18 +0000

    added coordinates for map markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -67,7 +67,26 @@ function initMap() {
 
         // Map Markers:
         
-        const myMark = {"lat": 54.1218, "lng": -0.9061};
+        const myMark = [
+                        // Attractions
+                        {"lat": 54.3063, "lng": -0.5997},
+                        {"lat": 54.021010, "lng": -1.000332},
+                        {"lat": 54.4012, "lng": -0.7125},
+
+                        // Museums
+                        {"lat": 54.1511, "lng": -0.7790},
+                        {"lat": 54.23361, "lng": -1.34485},
+                        {"lat": 53.9605, "lng": -1.0964},
+
+                        // Sights
+                        {"lat": 54.1218, "lng": -0.9061},
+                        {"lat": 54.2103, "lng": -2.3703},
+                        {"lat": 54.4883, "lng": -0.6079},
+
+                        // Walks
+                        {"lat": 54.2945, "lng": -1.9834},
+                        {"lat": 54.0803, "lng": -1.6850},
+                        {"lat": 54.0728, "lng": -2.1579}];
 
         const placeMarker = new google.maps.Marker({position: myMark});
 
