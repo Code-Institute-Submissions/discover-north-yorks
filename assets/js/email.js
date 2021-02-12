4b0d3b1834e5283da4668d84a39b5ff6e62265de
PaulFrankling <franky.p@hotmail.co.uk>
2021-02-12 19:05:09 +0000

    added email.js and linked it to contact form

---
+++ assets/js/email.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "discover north yorkshire", {
+        "from_firstname": contactForm.firstname.value,
+        "from_lastname": contactForm.lastname.value,
+        "from_email": contactForm.emailaddress.value,
+        "northyorkshire_query": contactForm.message.value
+    })
+    
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+}
